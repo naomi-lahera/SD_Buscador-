372a2b4eff1ca8608a5fa97c7d4692011f2ac91f
LeoRamirez1405 <leojavramirez1405@gmail.com>
2024-07-10 01:27:23 -0400

    falta definir las dicrecciones de las db y joblib

--- src/server/logic/models/retrieval_vectorial.py
+++ src/server/logic/models/retrieval_vectorial.py
@@ -1,24 +1,18 @@
-import os
-import sqlite3
-from joblib import load, dump
-from controller_bd import DocumentoController
 from gensim.corpora import Dictionary
 from gensim.models import TfidfModel
 from src.server.logic.build.preprocessing_data.preprocess import prepro
 from gensim.similarities import MatrixSimilarity
 from typing import List
+from src.server.data_access_layer.controller_bd import DocumentoController
 
-class Nodo():
-    def __init__(self):
-
-        read_or_create_db()
-        self.controller = DocumentoController()    
-
-    def retrieval(self, query):
-        id_tf_documents = self.controller.get_documents_for_query()
+class Retrieval_Vectorial():
+    def __init__():
+        pass
+    def retrieval(self, query,controller:DocumentoController):
+        id_tf_documents = controller.get_documents_for_query()
         
         # Crear un diccionario y corpus para el modelo TF-IDF
-        dictionary = self.controller.dictionary
+        dictionary = controller.dictionary
         corpus:List = [bow for _, bow in id_tf_documents]
         # Convertir las cadenas a listas de Python
         listas_convertidas = [[eval(item)] for item in corpus]
@@ -26,10 +20,6 @@ class Nodo():
         # Aplanar la lista
         corpus = [item for sublist in listas_convertidas for item in sublist]
 
-        
-        print("-----------------------------------")
-        print(dictionary)
-        print("-----------------------------------")
         model = TfidfModel(corpus)  # fit model
         
         # Tokenizar y convertir la consulta a BoW
@@ -64,47 +54,6 @@ class Nodo():
         
         # Obtener el ID del documento más relevante
         most_relevant_doc_id = id_tf_documents[max_sim_index][0]
-        document = self.controller.get_document_by_id(most_relevant_doc_id)
+        document = controller.get_document_by_id(most_relevant_doc_id)
         
         return document
-        
-def read_or_create_db():
-    
-    if os.path.exists('database.db'):
-        "La base de datos ya existe"
-        return 
-    else:
-        conexion = sqlite3.connect('database.db')
-    
-        cursor = conexion.cursor()
-        cursor.execute('''
-        CREATE TABLE IF NOT EXISTS documentos (
-        	id INTEGER PRIMARY KEY,
-        	texto_documento TEXT NOT NULL,
-        	tf TEXT
-        );
-        ''')
-        conexion.commit()
-        conexion.close()
-        print("La base de datos se creó correctamente")
-
-    
-     
-nodo = Nodo()
-# nodo.controller.create_document("Hi my name is leo")
-# nodo.controller.create_document("The air is fluid charming today")
-# nodo.controller.create_document("Computer Science is a big dream")
-# nodo.controller.create_document("Italy is a country from Asia")
-# print(nodo.controller.get_documents())
-# print("----------------------------------")
-# nodo.controller.update_document(4,"Italy is a country from Europe")
-# print(nodo.controller.get_documents())
-# print("----------------------------------")
-
-# nodo.controller.delete_document(2)
-# print(nodo.controller.get_documents())
-# print("----------------------------------")
-
-# nodo = Nodo()
-print(nodo.retrieval("honolulu Leo"))  
-# print(nodo.retrieval("I believe in the dream of all of us"))  
