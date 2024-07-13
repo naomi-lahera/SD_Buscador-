d879d3e6ba465affd13cf6a41746e71e1f7186b3
LeoRamirez1405 <leojavramirez1405@gmail.com>
2024-07-13 00:56:47 -0400

    bat modified

--- run.bat
+++ run.bat
@@ -4,5 +4,8 @@ setlocal enabledelayedexpansion
 :: Captura todos los argumentos pasados a run.bat
 set ARGUMENTOS=%*
 
+:: Usa %~dp0 para obtener el directorio completo del script actual
+set VOLUMEN_MONTAR=%~dp0
+
 :: Ejecuta el comando docker con los argumentos capturados
-docker run -it --rm -v "C:\Users\53527\Desktop\SD_Buscador-:/app" sist-dist:v0 /bin/bash -c "python3 src/server/__init__.py %ARGUMENTOS% && exec bash"
+docker run -it --rm -v "%VOLUMEN_MONTAR%:/app" sist-dist:v0 /bin/bash -c "python3 src/server/__init__.py %ARGUMENTOS% && exec bash"
